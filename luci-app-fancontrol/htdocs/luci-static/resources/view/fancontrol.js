d75cd373388dbae9547f769f4bb85bcffce04d6e
JiaY-shi <shi05275@163.com>
2024-02-27 11:06:26 +0800

    luci-app-fancontrol: add display of current temperature and speed

--- luci-app-fancontrol/htdocs/luci-static/resources/view/fancontrol.js
+++ luci-app-fancontrol/htdocs/luci-static/resources/view/fancontrol.js
@@ -2,11 +2,17 @@
 'require view';
 'require fs';
 'require form';
+'require uci';
 'require tools.widgets as widgets';
 
 return view.extend({
-	render: function () {
-		var m, s, o, v;
+	load: function () {
+		return Promise.all([
+			uci.load('fancontrol')
+		]);
+	},
+	render: async function (data) {
+		var m, s, o;
 
 		m = new form.Map('fancontrol', _('Fan General Control'));
 		s = m.section(form.TypedSection, 'fancontrol', _('Settings'));
@@ -17,9 +23,18 @@ return view.extend({
 
 		o = s.option(form.Value, 'thermal_file', _('Thermal File'), _('Thermal File'));
 		o.placeholder = '/sys/devices/virtual/thermal/thermal_zone0/temp';
+		var temp_div = uci.get('fancontrol', 'settings', 'temp_div');
+		var temp = parseInt(await fs.read_direct(uci.get('fancontrol', 'settings', 'thermal_file')));
+		if (temp_div > 0 && temp > 0) {
+			o.description = _('Current temperature:') + ' <b>' + (temp / temp_div) + '°C</b>';
+		} else {
+			o.description = _('Thermal File');
+		}
 
 		o = s.option(form.Value, 'fan_file', _('Fan File'), _('Fan Speed File'));
 		o.placeholder = '/sys/devices/virtual/thermal/cooling_device0/cur_state';
+		var speed = parseInt(await fs.read_direct(uci.get('fancontrol', 'settings', 'fan_file')));
+		o.description = _('Current speed:') + ' <b>' + (speed) + '</b>';
 
 		o = s.option(form.Value, 'start_speed', _('Initial Speed'), _('Please enter the initial speed for fan startup.'));
 		o.placeholder = '35';
